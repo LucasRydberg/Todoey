31667cfc1256df610eaf98db0f035f98569d1fae
Lucas <rydberg@slu.edu>
2018-08-28 18:07:40 -0500

    category view controller

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Lucas Rydberg on 8/28/18.
+//  Copyright © 2018 phas3. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadData()
+    }
+    
+    //MARK: TableView Datasource methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name!
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories.count
+    }
+    
+    
+    //MARK: TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // did select a row
+    }
+    
+    
+    //MARK: Data manipulation
+    func loadData(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching categories: \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func saveData(){
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: Add new category
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            // happenss when user clicks "Add" on the alert
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveData()
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -7,25 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     
     var itemArray = [Item]()
-    let preferences = UserDefaults.standard
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let newItem = Item(itemTitle: "go shopping")
-        itemArray.append(newItem)
-        
         loadItems()
-//        if let items = preferences.array(forKey: "itemArray") as? [Item] {
-//            itemArray = items
-//        }
     }
 
     
@@ -72,7 +66,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             // what happens when user clicks Add item button on the alert
-            let newItem = Item(itemTitle: textField.text!)
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             self.saveItems()
             
@@ -89,27 +86,57 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context: \(error)")
         }
+        
         tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("error \(error)")
+    // this method takes a parameter (request) but has a default value (Item.fetchRequest())
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context: \(error)")
+        }
+     
+        tableView.reloadData()
+    }
+    
+}
+
+// MARK: Searchbar methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // [cd] makes search case and diacritic insensitive
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(searchBar.text!.count == 0){
+            // happens when searchbar text goes empty
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
+            
         }
         
     }
+    
 }
 
